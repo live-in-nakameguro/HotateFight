b3d396337089447dd423b83e0b22cf296d004714
GitBaBYMan <hayaBBh2@gmail.com>
2021-07-03 12:52:03 +0900

    hokao_タイトル画面の作成

---
+++ Assets/Project/Scripts/ChangeScene.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+	public string keyDownCommand;
+	public string sceneName;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(keyDownCommand)) // enterが押された場合
+		{
+			SceneManager.LoadScene(sceneName);// シーンをロードする
+		}
+
+	}
+}
