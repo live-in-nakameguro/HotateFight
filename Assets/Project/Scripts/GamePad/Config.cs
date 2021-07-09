7227e104db39fa7d764603f6321a31a94204e8cc
live-in-nakameguro <sorakitahara2020@gmail.com>
2021-07-10 04:35:16 +0900

    sora_multi_ganepad

--- Assets/Project/Scripts/GamePad/Config.cs
+++ Assets/Project/Scripts/GamePad/Config.cs
@@ -16,52 +16,52 @@ namespace Gamepad.Config {
         public const float SLOW_VALUE_FOR_STICK = 1.0f / 10.0f;
 
         //LEFT_STICK
-        public const string LEFT_STICK_HORI = "LeftHorizontal";
-        public const string LEFT_STICK_VER = "LeftVertical";
+        public const string LEFT_STICK_HORI = "LeftHorizontal{0}";
+        public const string LEFT_STICK_VER = "LeftVertical{0}";
         public const float LEFT_STICK_HORI_MAX = 0.7f;
         public const float LEFT_STICK_HORI_MIN = -0.7f;
         public const float LEFT_STICK_VER_MAX = 0.6f;
         public const float LEFT_STICK_VER_MIN = -0.9f;
 
         //RIGHT_STICK
-        public const string RIGHT_STICK_HORI = "RightHorizontal";
-        public const string RIGHT_STICK_VER = "RightVertical";
+        public const string RIGHT_STICK_HORI = "RightHorizontal{0}";
+        public const string RIGHT_STICK_VER = "RightVertical{0}";
         public const float RIGHT_STICK_HORI_MAX = 0.7f;
         public const float RIGHT_STICK_HORI_MIN = -0.7f;
         public const float RIGHT_STICK_VER_MAX = 0.6f;
         public const float RIGHT_STICK_VER_MIN = -0.9f;
 
         //DAPD
-        public const string DPAD_HORI = "DpadHorizontal";
-        public const string DPAD_VER = "DpadVertical";
+        public const string DPAD_HORI = "DpadHorizontal{0}";
+        public const string DPAD_VER = "DpadVertical{0}";
         public const float DPAD_HORI_MAX = 1.0f;
         public const float DPAD_HORI_MIN = -1.0f;
         public const float DPAD_VER_MAX = 1.0f;
         public const float DPAD_VER_MIN = -1.0f;
 
         //A, B, X, Y
-        public const string BUTTON_A = "joystick button 1";
-        public const string BUTTON_B = "joystick button 0";
-        public const string BUTTON_X = "joystick button 3";
-        public const string BUTTON_Y = "joystick button 2";
+        public const string BUTTON_A = "joystick{0} button 1";
+        public const string BUTTON_B = "joystick{0} button 0";
+        public const string BUTTON_X = "joystick{0} button 3";
+        public const string BUTTON_Y = "joystick{0} button 2";
 
         //L, R
-        public const string BUTTON_L = "joystick button 4";
-        public const string BUTTON_R = "joystick button 5";
+        public const string BUTTON_L = "joystick{0} button 4";
+        public const string BUTTON_R = "joystick{0} button 5";
 
         //ZL, ZR
-        public const string BUTTON_ZL = "joystick button 6";
-        public const string BUTTON_ZR = "joystick button 7";
+        public const string BUTTON_ZL = "joystick{0} button 6";
+        public const string BUTTON_ZR = "joystick{0} button 7";
 
         //Z3, Z3
-        public const string BUTTON_L3 = "joystick button 10";
-        public const string BUTTON_R3 = "joystick button 11";
+        public const string BUTTON_L3 = "joystick{0} button 10";
+        public const string BUTTON_R3 = "joystick{0} button 11";
 
         //OPTIONS
-        public const string BUTTON_MINUS = "joystick button 8";
-        public const string BUTTON_PULS = "joystick button 9";
-        public const string BUTTON_PHOTO = "joystick button 13";
-        public const string BUTTON_HOME = "joystick button 12";
+        public const string BUTTON_MINUS = "joystick{0} button 8";
+        public const string BUTTON_PULS = "joystick{0} button 9";
+        public const string BUTTON_PHOTO = "joystick{0} button 13";
+        public const string BUTTON_HOME = "joystick{0} button 12";
     }
 
     public static class GamepadCameraConfig
