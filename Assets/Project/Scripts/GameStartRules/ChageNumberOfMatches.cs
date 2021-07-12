eee25b0b38624da417fdc7118300e3c0109ef6c5
live-in-nakameguro <sorakitahara2020@gmail.com>
2021-07-12 22:50:17 +0900

    sora_change_match_number
    sora_change_match_number

---
+++ Assets/Project/Scripts/GameStartRules/ChageNumberOfMatches.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChageNumberOfMatches : MonoBehaviour
+{
+    public Text TextFrame;
+
+    public int numberOfMatches = 3;
+
+    private int maxNumberOfMatches = 99;
+    private int minNumberOfMatches = 1;
+
+    public void countDownMatches()
+    {
+        numberOfMatches -= 1;
+        if (numberOfMatches < minNumberOfMatches)
+        {
+            numberOfMatches = maxNumberOfMatches;
+        }
+        TextFrame.text = numberOfMatches.ToString();
+    }
+
+    public void countUpMatches()
+    {
+        numberOfMatches += 1;
+        if (numberOfMatches > maxNumberOfMatches)
+        {
+            numberOfMatches = minNumberOfMatches;
+        }
+        TextFrame.text = numberOfMatches.ToString();
+    }
+
+}
