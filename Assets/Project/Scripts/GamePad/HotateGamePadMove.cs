7de8cff4cf141135dcf88938f9d5e90a094979e1
live-in-nakameguro <sorakitahara2020@gmail.com>
2021-08-12 02:32:54 +0900

    sora カメラ操作を直感的にする。

--- Assets/Project/Scripts/GamePad/HotateGamePadMove.cs
+++ Assets/Project/Scripts/GamePad/HotateGamePadMove.cs
@@ -11,6 +11,8 @@ public class HotateGamePadMove : MonoBehaviour
     //GamepadNumberの指定(0を選択した場合、すべてのゲームパッドで操作可能になる。)
     [SerializeField] int gamepadNumber = 0;
 
+    [SerializeField] GameObject horizontalRotNode;
+
     //キャラクターの操作状態を管理するフラグ
     private bool isFirstJumping = false;
     private bool isSecondJumping = false;
@@ -28,26 +30,30 @@ public class HotateGamePadMove : MonoBehaviour
     {
         PreventRotation();
 
-        //左スティックの左右で方向転換
+        //左スティックの上下で移動
         if (Input.GetAxis(SetGamepadNumber(GamepadButtonConfig.LEFT_STICK_VER)) >= (GamepadButtonConfig.LEFT_STICK_VER_MAX * GamepadButtonConfig.FAST_VALUE_FOR_STICK))
         {
             v = Time.deltaTime * GamepadHotateConfig.DASH_SPPED;
             _animator.SetBool("Running", true);
+            ResetCameraHorizontalPosition();
         }
         else if (Input.GetAxis(SetGamepadNumber(GamepadButtonConfig.LEFT_STICK_VER)) <= (GamepadButtonConfig.LEFT_STICK_VER_MIN * GamepadButtonConfig.FAST_VALUE_FOR_STICK))
         {
             v = -Time.deltaTime * GamepadHotateConfig.DASH_SPPED;
             _animator.SetBool("Running", true);
+            ResetCameraHorizontalPosition();
         }
         else if (Input.GetAxis(SetGamepadNumber(GamepadButtonConfig.LEFT_STICK_VER)) >= (GamepadButtonConfig.LEFT_STICK_VER_MAX * GamepadButtonConfig.SLOW_VALUE_FOR_STICK))
         {
             v = Time.deltaTime * GamepadHotateConfig.WALK_SPPED;
             _animator.SetBool("Running", true);
+            ResetCameraHorizontalPosition();
         }
         else if (Input.GetAxis(SetGamepadNumber(GamepadButtonConfig.LEFT_STICK_VER)) <= (GamepadButtonConfig.LEFT_STICK_VER_MIN * GamepadButtonConfig.SLOW_VALUE_FOR_STICK))
         {
             v = -Time.deltaTime * GamepadHotateConfig.WALK_SPPED;
             _animator.SetBool("Running", true);
+            ResetCameraHorizontalPosition();
         }
         else
         {
@@ -63,16 +69,20 @@ public class HotateGamePadMove : MonoBehaviour
 
         //左スティックの左右で方向転換
         if (Input.GetAxis(SetGamepadNumber(GamepadButtonConfig.LEFT_STICK_HORI)) >= (GamepadButtonConfig.LEFT_STICK_HORI_MAX * GamepadButtonConfig.SLOW_VALUE_FOR_STICK))
+        {
             h = Time.deltaTime * GamepadHotateConfig.ANGLE_CHAGE_SPPED;
-
+            ResetCameraHorizontalPosition();
+        }
         else if (Input.GetAxis(SetGamepadNumber(GamepadButtonConfig.LEFT_STICK_HORI)) <= (GamepadButtonConfig.LEFT_STICK_HORI_MIN * GamepadButtonConfig.SLOW_VALUE_FOR_STICK))
+        {
             h = -Time.deltaTime * GamepadHotateConfig.ANGLE_CHAGE_SPPED;
-
+            ResetCameraHorizontalPosition();
+        }
         else
             h = 0;
 
         //方向転換動作の実行
-        transform.Rotate(Vector3.up * h);
+        transform.Rotate(Vector3.up * h);　//Vector3.upとは( 0, 1, 0)のこと。　つまりy軸を中心にhだけ回転させてる。
     }
 
     //地面に接触したときにはonGroundをtrue、injumpingをfalseにする
@@ -96,6 +106,33 @@ public class HotateGamePadMove : MonoBehaviour
             isFirstJumping = true;
             isSecondJumping = false;
         }
+    }
+
+    //カメラの位置がデフォルトにない際に、移動キーで直感的にカメラの位置を戻すメソッド
+    private static float cameraMinDeg = 0;
+    private static float cameraMaxDeg = 360;
+    private float cameraMidDeg = (cameraMinDeg + cameraMaxDeg) / 2f;
+    private void ResetCameraHorizontalPosition()
+    {
+        float horizontalAngle = horizontalRotNode.transform.localEulerAngles.y;
+        float h = 0;
+        if (cameraMinDeg + 1 < horizontalAngle && horizontalAngle < cameraMidDeg)
+        {
+            horizontalRotNode.transform.localRotation = Quaternion.Euler(new Vector3(0, horizontalAngle - GamepadCameraConfig.VERTICAL_CAMERA_SPEED, 0));
+            h = Time.deltaTime * GamepadHotateConfig.ANGLE_CHAGE_SPPED;
+        }
+
+        else if (cameraMidDeg <= horizontalAngle && horizontalAngle < cameraMaxDeg - 1)
+        {
+            horizontalRotNode.transform.localRotation = Quaternion.Euler(new Vector3(0, horizontalAngle + GamepadCameraConfig.VERTICAL_CAMERA_SPEED, 0));
+            h = -Time.deltaTime * GamepadHotateConfig.ANGLE_CHAGE_SPPED;
+        }
+        else {
+            horizontalRotNode.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 0));
+        }
+
+        transform.Rotate(Vector3.up * h);
+
     }
 
     void Jump()
