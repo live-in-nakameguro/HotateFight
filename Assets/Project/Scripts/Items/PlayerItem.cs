e83a588ad1f26202e1f2f525f15fd362bb7f82a8
GitBaBYMan <hayaBBh2@gmail.com>
2021-08-16 21:38:57 +0900

    hokao_アイテムの基板実装

---
+++ Assets/Project/Scripts/Items/PlayerItem.cs
@@ -0,0 +1,80 @@
+using Gamepad.Config;
+using GameScenes.SettingAndResultBattle;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Item
+{
+    public class PlayerItem : MonoBehaviour
+    {
+        // GamepadNumberの指定(0を選択した場合、すべてのゲームパッドで操作可能になる。)
+        [SerializeField] int gamepadNumber = 0;
+
+        // アイテム画像表示
+        [SerializeField] Image image;
+
+        public static Dictionary<int, ItemSetting.Items> playerItemDict;
+
+        static PlayerItem()
+        {
+            SceneManager.sceneLoaded += Init;
+        }
+
+        private static void Init(Scene loadingScene, LoadSceneMode loadSceneMode)
+        {
+            playerItemDict = new Dictionary<int, ItemSetting.Items>()
+            {
+                { 1, ItemSetting.Items.None},
+                { 2, ItemSetting.Items.None},
+                { 3, ItemSetting.Items.None},
+                { 4, ItemSetting.Items.None}
+            };
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            ViewItem();
+            UseItem();
+        }
+
+        void ViewItem()
+        {
+            var sprite = ItemSetting.ItemTextureDict[playerItemDict[gamepadNumber]];
+            image.sprite = sprite;
+        }
+
+        void UseItem()
+        {
+            // アイテムを持っている場合
+            if (playerItemDict[gamepadNumber] != ItemSetting.Items.None)
+            {
+                if (Input.GetKeyDown(SetGamepadNumber(GamepadButtonConfig.BUTTON_Y)))
+                {
+                    Debug.Log("アイテム使用：" + playerItemDict[gamepadNumber]);
+                    playerItemDict[gamepadNumber] = ItemSetting.Items.None;
+                }
+            }
+        }
+
+        string SetGamepadNumber(string gamepadKey)
+        {
+            string gamepadNumberStr = "";
+            if (gamepadNumber != 0)
+            {
+                gamepadNumberStr = $" {gamepadNumber}";
+            }
+            //Debug.Log(string.Format(gamepadKey, gamepadNumberStr));
+            return string.Format(gamepadKey, gamepadNumberStr);
+        }
+    }
+}
