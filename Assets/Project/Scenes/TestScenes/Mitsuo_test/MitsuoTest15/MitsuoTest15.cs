8195f2458ebb41b3663ff8a65f4c57d5f4708f26
NS320 <tuscycling320@gmail.com>
2021-11-28 20:48:56 +0900

    mitsuo test commit
    descriotion test

---
+++ Assets/Project/Scenes/TestScenes/Mitsuo_test/MitsuoTest15/MitsuoTest15.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MitsuoTest15 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("１のスタート");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("１のアップデート");
+    }
+}
---
+++ Assets/Project/Scenes/TestScenes/Mitsuo_test/MitsuoTest15/MitsuoTest15_copy.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MitsuoTest15_copy : MonoBehaviour
+{
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("２のスタート");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("２のアップデート");
+    }
+}
